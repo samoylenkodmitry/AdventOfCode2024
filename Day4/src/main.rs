66a8a8f1318ecb6032c90af6d311ed6088659b13
Dmitry Samoylenko <samoylenkodmitry@gmail.com>
2024-12-04 23:39:42 +0300

    Day4

--- Day4/src/main.rs
+++ Day4/src/main.rs
@@ -24,31 +24,31 @@ fn f1(s: &str) -> usize {
     let g: Vec<Vec<_>> = s.lines().map(|l| l.bytes().collect()).collect();
     let mut cnt = 0;
     let p = [b'X', b'M', b'A', b'S', b'.'];
-    let dxy = [
-        (0, 1),
-        (1, 0),
-        (0, -1),
-        (-1, 0),
-        (-1, -1),
-        (1, 1),
-        (-1, 1),
-        (1, -1),
-    ];
-    for y in 0..g.len() {
-        for x in 0..g[0].len() {
-            if g[y][x] == b'X' {
-                for (dx, dy) in dxy {
+    for y in 1..=g.len() {
+        for x in 1..=g[0].len() {
+            if g[y - 1][x - 1] == b'X' {
+                for (dx, dy) in [
+                    (0, 1),
+                    (1, 0),
+                    (0, -1),
+                    (-1, 0),
+                    (-1, -1),
+                    (1, 1),
+                    (-1, 1),
+                    (1, -1),
+                ] {
                     let mut j = 0;
                     let (mut xx, mut yy) = (x, y);
-                    while j < 4 && g[yy][xx] == p[j] {
-                        j += 1;
-                        let x = xx as i32 + dx;
-                        let y = yy as i32 + dy;
-                        if x.min(y) < 0 || x >= g[0].len() as i32 || y >= g.len() as i32 {
-                            break;
-                        }
-                        xx = x as usize;
-                        yy = y as usize;
+                    while j < 4
+                        && yy > 0
+                        && yy <= g.len()
+                        && xx > 0
+                        && xx <= g[0].len()
+                        && g[yy - 1][xx - 1] == p[j]
+                    {
+                        xx = (xx as i32 + dx) as usize;
+                        yy = (yy as i32 + dy) as usize;
+                        j += 1
                     }
                     if j > 3 {
                         cnt += 1
@@ -66,11 +66,12 @@ fn f2(s: &str) -> usize {
     for y in 1..g.len() - 1 {
         for x in 1..g[0].len() - 1 {
             if g[y][x] == b'A' {
-                let a = g[y + 1][x + 1];
-                let b = g[y - 1][x - 1];
-                let c = g[y - 1][x + 1];
-                let d = g[y + 1][x - 1];
-                match [a, b, c, d] {
+                match [
+                    g[y + 1][x + 1],
+                    g[y - 1][x - 1],
+                    g[y - 1][x + 1],
+                    g[y + 1][x - 1],
+                ] {
                     [b'M', b'S', b'M', b'S'] => cnt += 1,
                     [b'M', b'S', b'S', b'M'] => cnt += 1,
                     [b'S', b'M', b'M', b'S'] => cnt += 1,
