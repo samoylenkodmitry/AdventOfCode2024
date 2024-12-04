a695fdf4a563a403a895391279236f07988746e5
Dmitry Samoylenko <samoylenkodmitry@gmail.com>
2024-12-04 22:23:32 +0300

    Day4

---
+++ Day4/src/main.rs
@@ -0,0 +1,140 @@
+#![feature(test)]
+
+extern crate test;
+use std::{fs::File, io::Read};
+use test::Bencher;
+
+fn main() {
+    let mut f = File::open("./4.txt").expect("can't open file");
+    let mut s = String::new();
+    f.read_to_string(&mut s).expect("can't read");
+    let a = f1(s.as_str());
+    let b = f2(s.as_str());
+    println!("{a} {b}")
+}
+
+#[bench]
+fn bench(b: &mut Bencher) {
+    b.iter(|| {
+        main();
+    })
+}
+
+fn f1(s: &str) -> usize {
+    let g: Vec<Vec<_>> = s.lines().map(|l| l.bytes().collect()).collect();
+    let mut cnt = 0;
+    let mut j = 0usize;
+    let p = [b'X', b'M', b'A', b'S', b'.'];
+    let dxy = [
+        (0, 1),
+        (1, 0),
+        (0, -1),
+        (-1, 0),
+        (-1, -1),
+        (1, 1),
+        (-1, 1),
+        (1, -1),
+    ];
+    let (ry, rx) = (0..g.len() as i32, 0..g[0].len() as i32);
+    for y in 0..g.len() as i32 {
+        for x in 0..g[0].len() as i32 {
+            if g[y as usize][x as usize] == b'X' {
+                for (dx, dy) in dxy {
+                    j = 0;
+                    let (mut xx, mut yy) = (x, y);
+                    while j < 4
+                        && rx.contains(&xx)
+                        && ry.contains(&yy)
+                        && g[yy as usize][xx as usize] == p[j]
+                    {
+                        xx += dx;
+                        yy += dy;
+                        j += 1
+                    }
+                    if j > 3 {
+                        cnt += 1
+                    }
+                }
+            }
+        }
+    }
+    cnt
+}
+
+fn f2(s: &str) -> usize {
+    let g: Vec<Vec<_>> = s.lines().map(|l| l.bytes().collect()).collect();
+    let mut cnt = 0;
+    let (ry, rx) = (0..g.len() as i32, 0..g[0].len() as i32);
+    for y in 0..g.len() as i32 {
+        for x in 0..g[0].len() as i32 {
+            if g[y as usize][x as usize] == b'A' {
+                let (xx, yy) = (x + 1, y + 1);
+                let a = if rx.contains(&xx) && ry.contains(&yy) {
+                    g[yy as usize][xx as usize]
+                } else {
+                    b'.'
+                };
+                let (xx, yy) = (x - 1, y - 1);
+                let b = if rx.contains(&xx) && ry.contains(&yy) {
+                    g[yy as usize][xx as usize]
+                } else {
+                    b'.'
+                };
+                let (xx, yy) = (x + 1, y - 1);
+                let c = if rx.contains(&xx) && ry.contains(&yy) {
+                    g[yy as usize][xx as usize]
+                } else {
+                    b'.'
+                };
+                let (xx, yy) = (x - 1, y + 1);
+                let d = if rx.contains(&xx) && ry.contains(&yy) {
+                    g[yy as usize][xx as usize]
+                } else {
+                    b'.'
+                };
+                match [a, b, c, d] {
+                    [b'M', b'S', b'M', b'S'] => cnt += 1,
+                    [b'M', b'S', b'S', b'M'] => cnt += 1,
+                    [b'S', b'M', b'M', b'S'] => cnt += 1,
+                    [b'S', b'M', b'S', b'M'] => cnt += 1,
+                    _ => {}
+                }
+            }
+        }
+    }
+    cnt
+}
+
+#[test]
+fn day2_part1() {
+    let s = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+    let a = f1(s);
+    let b = f1(include_str!(".././4.txt"));
+    println!("day4 part1: {a} {b}");
+}
+
+#[test]
+fn day2_part2() {
+    let s = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+    let a = f2(s);
+    let b = f2(include_str!(".././4.txt"));
+    println!("day4 part2: {a} {b}");
+}
